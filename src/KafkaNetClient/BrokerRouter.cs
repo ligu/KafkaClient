8b4c10caef32bb3ded0ff14764c9453692e0676a
erano.of <eran.of@gigya-inc.com>
2015-09-17 17:15:59 +0300

    refactor comments

--- src/KafkaNetClient/BrokerRouter.cs
+++ src/KafkaNetClient/BrokerRouter.cs
@@ -80,14 +80,9 @@ namespace KafkaNet
         /// Select a broker for a given topic using the IPartitionSelector function.
         /// </summary>
         /// <param name="topic">The topic to retreive a broker route for.</param>
-        /// <param name="key">
-        /// The key used by the IPartitionSelector to collate to a consistent partition. Null value
-        /// means key will be ignored in selection process.
-        /// </param>
+        /// <param name="key">The key used by the IPartitionSelector to collate to a consistent partition. Null value means key will be ignored in selection process.</param>
         /// <returns>A broker route for the given topic.</returns>
-        /// <exception cref="InvalidTopicNotExistsInCache">
-        /// Thrown if the topic metadata does not exist in the cache.
-        /// </exception>
+        /// <exception cref="InvalidTopicNotExistsInCache">Thrown if the topic metadata does not exist in the cache.</exception>
         public BrokerRoute SelectBrokerRouteFromLocalCache(string topic, byte[] key = null)
         {
             //get topic either from cache or server.
@@ -109,13 +104,10 @@ namespace KafkaNet
         /// <param name="topics">Collection of topics to request metadata for.</param>
         /// <returns>List of Topics as provided by Kafka.</returns>
         /// <remarks>
-        /// The topic metadata will by default check the cache first and then if it does not exist
-        /// it will then request metadata from the server. To force querying the metadata from the
-        /// server use <see cref="RefreshTopicMetadata"/>
+        /// The topic metadata will by default check the cache first and then if it does not exist it will then
+        /// request metadata from the server.  To force querying the metadata from the server use <see cref="RefreshTopicMetadata"/>
         /// </remarks>
-        /// <exception cref="InvalidTopicNotExistsInCache">
-        /// Thrown if the topic metadata does not exist in the cache.
-        /// </exception>
+        /// <exception cref="InvalidTopicNotExistsInCache">Thrown if the topic metadata does not exist in the cache.</exception>
         public List<Topic> GetTopicMetadataFromLocalCache(params string[] topics)
         {
             var topicSearchResult = SearchCacheForTopics(topics, null);
@@ -133,10 +125,8 @@ namespace KafkaNet
         /// </summary>
         /// <param name="topics">List of topics to update metadata for.</param>
         /// <remarks>
-        /// This method will ignore the cache and initiate a call to the kafka servers for all given
-        /// topics, updating the cache with the resulting metadata. Only call this method to force a
-        /// metadata update. For all other queries use <see cref="GetTopicMetadataFromLocalCache"/>
-        /// which uses cached values.
+        /// This method will ignore the cache and initiate a call to the kafka servers for all given topics, updating the cache with the resulting metadata.
+        /// Only call this method to force a metadata update.  For all other queries use <see cref="GetTopicMetadataFromLocalCache"/> which uses cached values.
         /// </remarks>
 
         public Task<bool> RefreshTopicMetadata(params string[] topics)
