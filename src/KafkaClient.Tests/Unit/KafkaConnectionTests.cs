01b5ce8722674bb7958aa21af398d735ad5a8be9
Andrew <awrobins@gmail.com>
2016-10-02 20:53:08 -0400

    fix test

--- src/KafkaClient.Tests/Unit/KafkaConnectionTests.cs
+++ src/KafkaClient.Tests/Unit/KafkaConnectionTests.cs
@@ -149,7 +149,7 @@ namespace KafkaClient.Tests.Unit
                 await TaskTest.WaitFor(() => receivedData);
 
                 //should log a warning and keep going
-                mockLog.Verify(x => x.WarnFormat(It.Is<string>(f => f == "Unexpected Response from {0} with CorrelationId={1} (not in request queue)."), 
+                mockLog.Verify(x => x.WarnFormat(It.Is<string>(f => f == "Unexpected Response from {0} with CorrelationId {1} (not in request queue)."), 
                     It.Is<object[]>(o => o != null && o.Length == 2 && (int)o[1] == correlationId)));
             }
         }
