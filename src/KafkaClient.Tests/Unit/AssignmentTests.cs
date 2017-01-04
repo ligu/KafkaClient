7c64efaaf0d7351f66ae02f43e0cd89a1af236d3
Andrew Robinson <awrobins@gmail.com>
2017-01-04 17:26:38 -0500

    another test

--- src/KafkaClient.Tests/Unit/AssignmentTests.cs
+++ src/KafkaClient.Tests/Unit/AssignmentTests.cs
@@ -44,6 +44,36 @@ namespace KafkaClient.Tests.Unit
         }
 
         [Test]
+        public async Task AssignmentFoundWhenStrategyExists([Values("type1", "type2")] string strategy)
+        {
+            var metadata = new ConsumerProtocolMetadata("mine", strategy);
+
+            var router = Substitute.For<IRouter>();
+            var conn = Substitute.For<IConnection>();
+            router.GetGroupBrokerAsync(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                  .Returns(_ => Task.FromResult(new GroupBroker(_.Arg<string>(), 0, conn)));
+            conn.SendAsync(Arg.Any<JoinGroupRequest>(), Arg.Any<CancellationToken>(), Arg.Any<IRequestContext>())
+                  .Returns(_ => Task.FromResult(new JoinGroupResponse(ErrorResponseCode.None, 1, metadata.AssignmentStrategy, _.Arg<JoinGroupRequest>().MemberId, _.Arg<JoinGroupRequest>().MemberId, new []{ new JoinGroupResponse.Member(_.Arg<JoinGroupRequest>().MemberId, metadata) })));
+            conn.SendAsync(Arg.Any<SyncGroupRequest>(), Arg.Any<CancellationToken>(), Arg.Any<IRequestContext>())
+                  .Returns(_ => Task.FromResult(new SyncGroupResponse(ErrorResponseCode.None, new ConsumerMemberAssignment())));
+
+            var assignor = Substitute.For<IMembershipAssignor>();
+            assignor.AssignmentStrategy.ReturnsForAnyArgs(_ => strategy);
+            var encoders = ConnectionConfiguration.Defaults.Encoders(new ConsumerEncoder(new ConsumerAssignor(), assignor));
+            var consumer = new Consumer(router, encoders: encoders);
+            using (consumer) {
+                using (var member = await consumer.JoinConsumerGroupAsync("group", ConsumerEncoder.Protocol, metadata, CancellationToken.None)) {
+                    await consumer.SyncGroupAsync(
+                            member.GroupId, member.MemberId, member.GenerationId, member.ProtocolType,
+                            ImmutableDictionary<string, IMemberMetadata>.Empty.Add(
+                                metadata.AssignmentStrategy, metadata),
+                            CancellationToken.None);
+                }
+            }
+        }
+
+
+        [Test]
         public async Task AssignmentSucceedsWhenStrategyExists([Values("type1", "type2")] string strategy)
         {
             var metadata = new ConsumerProtocolMetadata("mine", strategy);
