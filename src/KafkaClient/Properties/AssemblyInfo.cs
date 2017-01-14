3412d84997d77b6f4e4f39e0378de9b932f63a40
Andrew Robinson <awrobins@gmail.com>
2017-01-13 21:46:47 -0500

    use dotnet pack instead of nuget pack, while we're using project.json approach

--- src/KafkaClient/Properties/AssemblyInfo.cs
+++ src/KafkaClient/Properties/AssemblyInfo.cs
@@ -4,13 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("KafkaClient")]
 [assembly: AssemblyDescription("Native C# client for Apache Kafka.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nudge Software Inc")]
 [assembly: AssemblyProduct("KafkaClient")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("Nudge Software 2016")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
 
 [assembly: InternalsVisibleTo("KafkaClient.Tests")]
