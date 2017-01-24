e2d705b7af9801fc9123e0b9cbaff4ebd52ea2af
Andrew Robinson <awrobins@gmail.com>
2017-01-24 00:48:46 -0500

    Failed experiment -- saved for posterity :)

--- src/KafkaClient/Common/Compression.cs
+++ src/KafkaClient/Common/Compression.cs
@@ -27,62 +27,75 @@ namespace KafkaClient.Common
 
         public static ArraySegment<byte> Unzip(this ArraySegment<byte> source)
         {
-            ArraySegment<byte> known;
+#if false
             using (var writer = new KafkaWriter()) {
                 using (var gzip = new GZipStream(new MemoryStream(source.Array, source.Offset, source.Count), CompressionMode.Decompress)) {
                     gzip.CopyTo(writer.Stream);
                     gzip.Flush();
                 }
-                known = writer.ToSegment();
+                return writer.ToSegment();
             }
+#elif false
 
-            ArraySegment<byte> unknown;
-            using (var unzipped = new MemoryStream(CopyBufferSize)) {
-                using (var gzip = new GZipStream(new MemoryStream(source.Array, source.Offset, source.Count), CompressionMode.Decompress)) {
-                    unzipped.Write(0.ToBytes(), 0, KafkaEncoder.IntegerByteSize); // placeholder for length
-
-                    ArraySegment<byte> bytes;
-                    int bytesWritten;
-                    do {
-                        var capacity = unzipped.Capacity;
-                        var offset = (int)unzipped.Position;
-                        if (capacity < offset + CopyBufferSize / 2) {
-                            capacity += CopyBufferSize;
-                            unzipped.SetLength(capacity); // setting capacity rather than length wipes the underlying buffer
-                        }
-                        unzipped.TryGetBuffer(out bytes);
-                        bytesWritten = gzip.Read(bytes.Array, offset, capacity - offset);
-                        unzipped.Seek(bytesWritten, SeekOrigin.Current);
-                    } while (bytesWritten > 0);
-
-                    if (unzipped.Position > KafkaEncoder.IntegerByteSize) {
-                        var totalBytesWritten = (int) unzipped.Position;
-                        unzipped.Seek(0, SeekOrigin.Begin);
-                        unzipped.Write((totalBytesWritten - KafkaEncoder.IntegerByteSize).ToBytes(), 0, 4); // actual length
-                        unknown = new ArraySegment<byte>(bytes.Array, 0, totalBytesWritten);
-                    } else {
-                        unzipped.TryGetBuffer(out bytes);
-                        //return bytes;
-                        unknown = bytes;
+            var bytes = new byte[CopyBufferSize];
+            var offset = KafkaEncoder.IntegerByteSize; // placeholder for length
+            using (var gzip = new GZipStream(new MemoryStream(source.Array, source.Offset, source.Count), CompressionMode.Decompress)) {
+                int bytesWritten;
+                do {
+                    if (bytes.Length < offset + CopyBufferSize / 2) {
+                        var expandedBytes = new byte[bytes.Length + CopyBufferSize];
+                        Buffer.BlockCopy(bytes, KafkaEncoder.IntegerByteSize, expandedBytes, KafkaEncoder.IntegerByteSize, offset - KafkaEncoder.IntegerByteSize);
+                        bytes = expandedBytes;
                     }
+                    bytesWritten = gzip.Read(bytes, offset, bytes.Length - offset);
+                    offset += bytesWritten;
+                } while (bytesWritten > 0);
+
+                var lengthBytes = (offset - KafkaEncoder.IntegerByteSize).ToBytes(); 
+                Buffer.BlockCopy(lengthBytes, 0, bytes, 0, KafkaEncoder.IntegerByteSize);  // actual length
+                return new ArraySegment<byte>(bytes, 0, offset);
+            }
+#else
+            var uncompressedLength = 0;
+            using (var gzip = new GZipStream(new MemoryStream(source.Array, source.Offset, source.Count), CompressionMode.Decompress)) {
+                var bytes = new byte[CopyBufferSize + KafkaEncoder.IntegerByteSize];
+                int bytesWritten, offset = KafkaEncoder.IntegerByteSize;
+                do {
+                    bytesWritten = gzip.Read(bytes, offset, bytes.Length - offset);
+                    if (offset + bytesWritten < bytes.Length) {
+                        offset += bytesWritten;
+                    } else {
+                        offset = 0;
+                    }
+                    uncompressedLength += bytesWritten;
+                } while (bytesWritten > 0);
+
+                if (uncompressedLength <= CopyBufferSize) {
+                    // it fit in the first buffer, so no need to reallocate
+                    offset = 0;
+                    foreach (var lengthByte in uncompressedLength.ToBytes()) {
+                        bytes[offset++] = lengthByte;
+                    }
+                    return new ArraySegment<byte>(bytes, 0, KafkaEncoder.IntegerByteSize + uncompressedLength);
                 }
             }
 
-            var message = "";
-            if (known.Count != unknown.Count) {
-                message = $"Expected {known.Count} but got {unknown.Count} bytes\n";
-            }
-            var lesser = Math.Min(known.Count, unknown.Count);
-            for (var i = 0; i < lesser; i++) {
-                if (known.Array[i] != unknown.Array[i]) {
-                    message += $"Expected {known.Array[i]} but got {unknown.Array[i]} at index {i}\n";
-                    break;
+            using (var gzip = new GZipStream(new MemoryStream(source.Array, source.Offset, source.Count), CompressionMode.Decompress)) {
+                var lengthBytes = uncompressedLength.ToBytes(); 
+                var bytes = new byte[uncompressedLength + lengthBytes.Length];
+                var offset = 0;
+                foreach (var lengthByte in uncompressedLength.ToBytes()) {
+                    bytes[offset++] = lengthByte;
                 }
+
+                int bytesWritten;
+                do {
+                    bytesWritten = gzip.Read(bytes, offset, bytes.Length - offset);
+                    offset += bytesWritten;
+                } while (bytesWritten > 0);
+                return new ArraySegment<byte>(bytes);
             }
-
-            if (message.Length > 0) throw new BufferUnderRunException(message);
-
-            return known;
+#endif
         }
     }
 }
